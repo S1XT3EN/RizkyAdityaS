6739535204d4556cb9770baedcd0e5a191ee4a4a
Roxum user <roxum@local>
2026-09-04 12:03:36 +0700

    feat & refactor: overhaul portfolio design with floating nav, 3D theme, and modular structure

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function toggleMenu() {
     navbar.classList.toggle('active');
     if (navbar.classList.contains('active')) {
         menuIcon.classList.remove('fa-bars');
-        menuIcon.classList.add('fa-xmark');
+        ('fa-xmark');
     } else {
         menuIcon.classList.remove('fa-xmark');
         menuIcon.classList.add('fa-bars');
